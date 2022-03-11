8c89cce52a37ab3a413b535b462f72fa35ee5aa3
张颖3 <zhangying3@bsoft.com.cn>
2022-03-11 10:30:50 +0800

    配置修改

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.scss'
+class LoginPage extends Component{
+    render() {
+        return (
+            <div className="box">
+                Hello,React,10k
+            </div>
+
+        );
+    }
+}
+
+export default LoginPage
